885bc8dc23ad7ea9ea884fe9b6233fca5a8ac6db
superBunger <120007693+superBunger@users.noreply.github.com>
2024-01-11 19:44:05 +0100

    Försöker få inventory display att funka

--- Assets/Scripts/furnitureStats.cs
+++ Assets/Scripts/furnitureStats.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class furnitureStats : MonoBehaviour
 {
     public int furnitureCost;
-    public int furnitureStat1;
-    public int furnitureStat2;
-    public int furnitureStat3;
+    public int furnitureComfort;
+    public int furnitureDesign;
+    public int furnitureAtmosphere;
 }
