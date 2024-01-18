4585e85db686c82b8f4e3bc84d84e38e93d0c3da
ronnie <ronnie.grannas@elev.ga.lbs.se>
2024-01-18 14:15:39 +0100

    klocka klar
    kan identifiera perioden och nästan klar med att kolla om det ska regna

--- Assets/Scripts/ClockUI.cs
+++ Assets/Scripts/ClockUI.cs
@@ -6,13 +6,15 @@ using TMPro;
 
 public class ClockUI : MonoBehaviour
 {
-
+    public enum Period { morning, afternoon, night}
+    public Period currentPeriod;
     private const float REAL_SECONDS_PER_INGAME_DAY = 960f;
     private const float afternoon_length = 360f;
     private const float morning_length = 360f;
     private const float daytime_length = afternoon_length + morning_length;
     private const float nighttime_length = 240f;
     public bool isday = true;
+    public bool israining = false;
 
 
     private Transform clockHandTransform;
@@ -27,17 +29,38 @@ public class ClockUI : MonoBehaviour
         dayText.text = "Day " + numDays;
     }
 
+    public void test()
+    {
+        print("maybe rain?");
+
+    }
+
     private void Update()
     {
         if (isday == true)
         {
             day += Time.deltaTime / daytime_length;
+            if (day < 0.5f && currentPeriod != Period.morning)
+            {
+                currentPeriod = Period.morning;
+                test();
+            } else if (day > 0.5f && currentPeriod != Period.afternoon)
+            {
+                currentPeriod = Period.afternoon;
+                test();
+            }
         }
         else
         {
+            if (currentPeriod != Period.night)
+            {
+                currentPeriod = Period.night;
+                test();
+            }
             day += Time.deltaTime / nighttime_length;
+            
         }
-        
+
 
         float dayNormalized = day % 1f;
 
