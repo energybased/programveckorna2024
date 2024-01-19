a28d9e028cdb06b168e606a4e99c8a3b816e4d4e
ItsMeHappy <120007989+ItsMeHappy@users.noreply.github.com>
2024-01-19 12:10:09 +0100

    Kund

---
+++ Assets/Scripts/KundAI.cs
@@ -0,0 +1,117 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+
+public class KundAI : MonoBehaviour
+{
+    public Transform Till;
+    public Transform pickUp;
+    public Transform emptyTable;
+
+    public float speed = 200f;
+    public float nextWayPointDistance = 3f;
+    
+    public int currentWaypoint;
+
+    bool hasOrdered = false;
+    bool hasPickedUp = false;
+
+    Path path;
+    
+    
+    bool ReachedEndOfPath = false;
+
+    Seeker seeker;
+    Rigidbody2D rb2d;
+    void Start()
+    {
+        seeker = GetComponent<Seeker>();
+        rb2d = GetComponent<Rigidbody2D>();
+
+        if(hasOrdered == false && hasPickedUp == false)
+        {
+            seeker.StartPath(rb2d.position, Till.position, OnPathComplete);
+            Debug.Log("Going to Till");
+        }   
+    }
+
+    void OnPathComplete(Path p)
+    {
+        if(!p.error)
+        {
+            path = p;
+            currentWaypoint = 0;
+        }
+    }
+
+   
+    void FixedUpdate()
+    {
+        if(path == null)
+        {
+            return;
+        }
+
+        if(currentWaypoint >= path.vectorPath.Count)
+        {
+            ReachedEndOfPath = true;
+            return;
+        }
+        else
+        {
+            ReachedEndOfPath = false;
+        }
+
+            Vector2 direction = ((Vector2)path.vectorPath[currentWaypoint] - rb2d.position).normalized;
+            Vector2 force = direction * speed * Time.deltaTime;    
+
+            rb2d.AddForce(force);
+
+            float distance = Vector2.Distance(rb2d.position, path.vectorPath[currentWaypoint]);
+
+        if(distance < nextWayPointDistance)
+        {
+            currentWaypoint++;
+        }
+    }
+    void OnTriggerEnter2D (Collider2D Collision)
+    {
+        
+        if(Collision.gameObject.tag == "Arbetare" && hasOrdered == false && hasPickedUp == false)
+        {
+            StartCoroutine(CoffeeOrder());  
+        }
+
+        if(Collision.gameObject.tag == "Arbetare" && hasOrdered == true && hasPickedUp == false)
+        {
+            CoffeePickUp();
+        }
+    }   
+    IEnumerator CoffeeOrder()
+        {
+            Debug.Log("Ordering Coffee");
+            path = null;
+            yield return new WaitForSeconds(5);
+            hasOrdered = true;
+            seeker.StartPath(rb2d.position, pickUp.position, OnPathComplete);
+            Debug.Log("Going to PickUp");
+        }
+
+    IEnumerator DeSpawn()
+    {
+        yield return new WaitForSeconds(20);
+        Destroy(gameObject);
+    }
+
+    void CoffeePickUp()
+        {
+            hasPickedUp =  true;
+            path = null;
+            seeker.StartPath(rb2d.position, emptyTable.position, OnPathComplete);
+            Debug.Log("Going to Exit");
+            Debug.Log("Has picked up order");
+            StartCoroutine(DeSpawn());
+        }  
+}
--- Assets/Scripts/KundSpawner.cs
+++ Assets/Scripts/KundSpawner.cs
@@ -7,10 +7,19 @@ public class KundSpawner : MonoBehaviour
     
     public GameObject kundPrefab;
     [SerializeField]
+<<<<<<< Updated upstream
+=======
+
+    float counter = 0;
+    int counterSpeed;
+
+    [SerializeField]
+>>>>>>> Stashed changes
     int NumberOfKunder = 0;
 
     void Start()
     {
+<<<<<<< Updated upstream
         
     }
 
@@ -30,4 +39,42 @@ public class KundSpawner : MonoBehaviour
         Instantiate(kundPrefab, new Vector3(-0.5f, -8, 1), Quaternion.identity);
         NumberOfKunder += 1;
     }
+=======
+        counterSpeed = 1;
+    }
+   // Update is called once per frame
+    void FixedUpdate()
+    {
+        counter += counterSpeed * Time.deltaTime;
+        float randomNumber1= Random.Range(0,2);
+        float randomNumber= Random.Range(0,2);
+
+        if(counter >= 30 && NumberOfKunder <= 10 && randomNumber == 1 )
+        {
+            
+            if(randomNumber1 == 0)
+            {
+                AstarPath.active.Scan();
+                counter = 0;
+                Instantiate(kundPrefab, transform.position,transform.rotation);
+                NumberOfKunder += 1;
+
+            }
+            else
+            {
+                counter = 20;
+            }
+            //else if(randomNumber1 == 1)
+           // {
+             //   counter = 0;
+             //   Instantiate(kundPrefab, transform.position,transform.rotation);
+             //   Instantiate(kundPrefab, transform.position,transform.rotation);
+              //  NumberOfKunder += 2;
+          //  }
+            
+        }
+    }
+
+    
+>>>>>>> Stashed changes
 }
