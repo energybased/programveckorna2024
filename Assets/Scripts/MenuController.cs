d0797fe184f5cb8e0e0387e6f9346f024e7d795a
ronnie <ronnie.grannas@elev.ga.lbs.se>
2024-02-22 12:54:18 +0100

    ja
    små ändringar, fortsätter hemma

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -3,9 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class MenuController : MonoBehaviour
 {
+    [Header("Volume Settings")]
+    [SerializeField] private TMP_Text VolumeSliderValue = null;
+    [SerializeField] private Slider VolumeSlider = null;
+
+    [Header("Confirmation")]
+    [SerializeField] private GameObject ConfirmationPrompt = null;
+
+
+    [Header("Main menu")]
     public string _newGameLevel;
     private string levelToLoad;
     [SerializeField] private GameObject NoSavePan = null;
@@ -34,4 +44,22 @@ public class MenuController : MonoBehaviour
         Application.Quit();
     }
 
+    public void SetVolume(float volume)
+    {
+        AudioListener.volume = volume;
+        VolumeSliderValue.text = volume.ToString("000");
+    }
+
+    public void VolumeApply()
+    {
+        PlayerPrefs.SetFloat("MasterVolume", AudioListener.volume);
+        //StartCoroutine(ConfimationBox());
+    }
+
+    public IEnumerable ConfirmationBox()
+    {
+        ConfirmationPrompt.SetActive(true);
+        yield return new WaitForSeconds(2);
+        ConfirmationPrompt.SetActive(false);
+    }
 }
